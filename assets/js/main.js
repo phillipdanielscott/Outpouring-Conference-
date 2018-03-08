b531762e181c204941a66eb84eb37ea0f8808401
Phillip  Scott <phillipd20@yahoo.com>
2018-03-08 08:03:33 -0500

    added page numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,13 @@ $(document).ready(function() {
             $(this).fadeIn();
         });
     });
-
+    $('.fraction').each(function(key, value) {
+        $this = $(this)
+        var split = $this.html().split("/")
+        if (split.length == 2) {
+            $this.html('<span class="top">' + split[0] + '</span><span class="bottom">' + split[1] + '</span>')
+        }
+    });
 
 });
 
